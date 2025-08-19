066ea2b7c59490e0b14a9ba39383fdd0e3d2e21e
Sri-Sharan-Prakash <srisharanprakash2003@gmail.com>
2025-08-19 19:55:17 +0530

    experience

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 const Experience = () => {
+  const experience = [{name:"Full Stack Developer Intern",image:"https://media.licdn.com/dms/image/v2/D4D0BAQH_IP1079LmJw/company-logo_200_200/B4DZfJ4.i.GgAM-/0/1751438846907/skillminetech_logo?e=2147483647&v=beta&t=KDX34xzIxdJ6Q20ZKvWoEmtqu4xN5hLJ_SJZ21ODpRI",description:"Worked in their major product comPLYment where I have contributed to the development of key features and improvements. Had developed admin-side functionalities like CRUD in both frontend and backend.", company:"Skillmine Technologies Pvt Ltd", duration:"Jan 2025 - July 2025",stipend:"7500"}]
   return (
     <div className='mt-23'>
         <div>
             <h1 className='text-3xl font-medium text-white text-center'>Experience</h1> 
+        </div>
+        <div className='mt-10'>
+            {experience.map((exp, index) => (
+                <div key={index} className='flex mb-5 bg-gray-900 gap-5 p-4 rounded-lg'>
+                  <div>
+                    <img src={exp.image} className='w-15 rounded-2xl' alt="" />
+                  </div>
+                  <div>
+                    <h2 className='text-xl font-semibold text-white'>{exp.name}</h2>
+                    <h2 className='text-white'>{exp.description}</h2>
+                    <p className='text-gray-400'>{exp.company}</p>
+                    <p className='text-gray-400'>{exp.duration}</p>
+                    <p className='text-gray-100 bg-purple-500 rounded w-fit font-medium px-4 mt-2 py-1'>Stipend: ₹ {exp.stipend}</p>
+                  </div>
+                </div>
+            ))}
         </div>
     </div>
   )
--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -5,7 +5,7 @@ const Skills = () => {
   const skills=[
     {name:"Frontend",svg:"https://lottie.host/b99b8bae-a6ad-442e-853a-6bae99b58818/Mocuf0Zm2J.lottie",tools:[{name:"React",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"},{name:"HTML",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},{name:"Tailwind CSS",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"},{name:"CSS",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},{name:"JavaScript",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"},{name:"Bootstrap",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"},{name:"typescript",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"}]},
     {name:"Backend",svg:"https://lottie.host/db8694a0-9acd-4d8d-9e63-1813531d8f78/XMEja70kuW.lottie",tools:[{name:"Node.js",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"},{name:"Express",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"},{name:"MongoDB",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"},{name:"Java",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"},{name:"Python",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"},{name:"Nest JS",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nestjs/nestjs-original.svg"}]},
-    {name:"Others",tools:[{name:"Git",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"},{name:"Docker",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"},{name:"Figma",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"},{name:"Postman",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg"},{name:"Github",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"},{name:"Git Lab",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg"}]}
+    {name:"Others",svg:"https://lottie.host/ee85dbf0-0c78-4c40-adb1-3dfb3f3f6ee0/M91tedvT5X.lottie",tools:[{name:"Git",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"},{name:"Docker",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"},{name:"Figma",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"},{name:"Postman",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg"},{name:"Github",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"},{name:"Git Lab",image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg"}]}
   ]
   return (
     <div>
@@ -17,7 +17,7 @@ const Skills = () => {
             {skills.map((skill) => (
                 <div key={skill.name} className='bg-[#1E1E2F] p-5 hover:scale-105 rounded-lg w-80 border-1 drop-shadow-purple-500 border-purple-500/20 hover:border-purple-500 transition duration-300 flex flex-col'>
                     <h2 className='text-white font-semibold text-lg text-center'>{skill.name}</h2>
-                    <div>
+                    <div className="mt-1">
                       <DotLottieReact
                           src={skill.svg}
                           loop
